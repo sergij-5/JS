31cbc2950356a7f411cd6c68e49bc83dfeabf700
sergij-5 <sergijvronskij5@gmail.com>
2025-10-19 21:03:18 +0300

    js 1.5

---
+++ JS 1/scripts/main-numbers.js
@@ -0,0 +1,23 @@
+
+
+
+let a = 100500; // integer
+console.log(a);
+
+let b = 3.123424; // float double
+console.log(b);
+
+console.log(10+20);
+console.log(10-20);
+console.log(10*20);
+console.log(10/20);
+console.log(11 % 3);
+
+console.log(0.1 + 0.2);
+
+let big = BigInt(1234567899874567895478854864);
+console.log(big);
+
+let n1 = 100;
+let n2 = 200;
+console.log(n1 + n2);
