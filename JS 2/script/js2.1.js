8266ec034834127ea8e24772b2173c168b9c4082
sergij-5 <sergijvronskij5@gmail.com>
2025-10-30 21:18:41 +0200

    js 2

---
+++ JS 2/script/js2.1.js
@@ -0,0 +1,15 @@
+
+
+let arr;
+arr = [123,234,345,-123,0, true, 'dfvbdhb'];
+console.log(arr);
+console.log(arr[0]);
+console.log(arr[2]);
+console.log(arr[4]);
+console.log(arr.length);
+
+arr[0] = 'new value';
+console.log(arr);
+
+arr[7] = 'okten'
+console.log(arr);
